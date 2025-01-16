69fb0b85bca9beeff88275c62c38f0877be8bbac
harsuuu <harshjoshi7895@gmail.com>
2025-01-16 22:43:37 +0530

    all components

---
+++ src/data.js
@@ -0,0 +1,47 @@
+const reviews = [
+  {
+    id: 1,
+    name: "Harsh Joshi",
+    job: "B.Tech final-year student",
+    image: "https://media.licdn.com/dms/image/v2/D5603AQHC45YSBa0p9A/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1691413476443?e=1742428800&v=beta&t=gGyVe5YUd0YEb6x6In-0XGqJRDF5rfZrAWrpORZFagE",
+    text: "I am a B.Tech final-year student, actively seeking opportunities in the tech industry.I have honed my skills in C,C++,Java,Python,PHP,&front-end development.I am eager to bring my knowledge and passion for contributing effectively and driving real results.",
+  },
+  {
+    id: 2,
+    name: "Aditya Rawat",
+    job: "B.Tech final-year student",
+    image: "https://media.licdn.com/dms/image/v2/D4E03AQFU9ey7y-QCLA/profile-displayphoto-shrink_400_400/profile-displayphoto-shrink_400_400/0/1695538579630?e=1742428800&v=beta&t=5gvrQgipY-0MPby8ivzGBLRJXc1QypDb0HN3e9jQfkE",
+    text: "I am a B.Tech final-year student with a CGPA above 9, actively seeking job opportunities in the tech industry. Over the years, I have honed my skills in C, C++, Java, and Python, and I am passionate about development.",
+  },
+  {
+    id: 3,
+    name: "Sarthak Singh Chaudhary",
+    job: "B.Tech final-year student",
+    image: "https://media.licdn.com/dms/image/v2/D4D03AQF5ku0mPxY7mg/profile-displayphoto-shrink_400_400/profile-displayphoto-shrink_400_400/0/1676468079355?e=1742428800&v=beta&t=rss9AOsqEveeH4dj16EdAiVZyfvCuGbrVziaOZfIdL0",
+    text: "I am a B.Tech final-year student, actively seeking job opportunities in the tech industry. Over the years, I have honed my skills in C, C++, Java, and Python, and I am passionate about development.",
+  },
+  {
+    id: 4,
+    name: "Abhishek Chauhan",
+    job: "B.Tech final-year student",
+    image: "http://127.0.0.1:5500/chapter%201/index.html",
+    text: "I am a B.Tech final-year student with a CGPA above 9.5, actively seeking job opportunities in the tech industry.I have honed my skills in C, C++, Java, Python, PHP, and machine learning.",
+  },
+  {
+    id: 5,
+    name: "Akshit Chaudhary",
+    job: "B.Tech final-year student",
+    image: "https://media.licdn.com/dms/image/v2/D4D03AQGIYIyzGc5Rjg/profile-displayphoto-shrink_400_400/profile-displayphoto-shrink_400_400/0/1694120687047?e=1742428800&v=beta&t=V8cbFnGXbH05z8J7o_WYC-F4v4vcbpD_cUpwc-5No9c",
+    text: "I am a B.Tech final-year student, actively seeking job opportunities in the tech industry. Over the years, I have honed my skills in C, C++, Java, and Python, and I am passionate about development.",
+  },
+  {
+    id: 6,
+    name: "Ayush Semwal",
+    job: "B.Tech final-year student",
+    image: "http://127.0.0.1:5500/ayush.jpg",
+    text: "I am a B.Tech final-year student, actively seeking job opportunities in the tech industry. Over the years, I have honed my skills in C, C++, Java, and Python, and I am passionate about development.",
+  },
+ 
+];
+
+export default reviews;
